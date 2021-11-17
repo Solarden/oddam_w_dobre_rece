13f175bd96534816d7ab1f8d6b787c7bd3968e27
solarden <pchraczynski@gmail.com>
2021-11-17 22:36:05 +0100

    anchor links update

--- oddam_w_dobre_rece/mainapp/admin.py
+++ oddam_w_dobre_rece/mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from mainapp import models
 # Register your models here.
+
+admin.site.register(models.Donation)
+admin.site.register(models.Category)
+admin.site.register(models.Institution)
--- oddam_w_dobre_rece/mainapp/views.py
+++ oddam_w_dobre_rece/mainapp/views.py
@@ -3,10 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
+from mainapp.models import Donation
+
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        stat_passed_bags = 0
+        for item in Donation.objects.all():
+            stat_passed_bags += item.quantity
+        stat_supported_institutions = Donation.objects.all().count()
+        return render(request, 'index.html',
+                      {'stat_bags': stat_passed_bags, 'stat_institutions': stat_supported_institutions})
 
 
 class AddDonation(View):
