e9f8a545ffda9a907cf417403b033e6335590cc1
Pawel Ch <pchraczynski@gmail.com>
2021-12-01 13:24:18 +0100

    admin list wip

--- oddam_w_dobre_rece/mainapp/admin.py
+++ oddam_w_dobre_rece/mainapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+
 from mainapp import models
+from django.db import models as db_models
 
 
 class BaseAdmin(admin.ModelAdmin):
@@ -12,6 +16,25 @@ class BaseAdmin(admin.ModelAdmin):
             return False
 
 
+class DummyModel(db_models.Model):
+    class Meta:
+        verbose_name_plural = 'Lista administratorów'
+        app_label = 'mainapp'
+
+
+def my_custom_view(request):
+    return HttpResponse('test')
+
+
+class DummyModelAdmin(admin.ModelAdmin):
+    model = DummyModel
+
+    def get_urls(self):
+        view_name = '{}_{}_changelist'.format(self.model._meta.app_label, self.model._meta.model_name)
+        return [path('admin/', my_custom_view, name=view_name)]
+
+
+admin.site.register(DummyModel, DummyModelAdmin)
 admin.site.register(models.Donation)
 admin.site.register(models.Category)
 admin.site.register(models.Institution)
