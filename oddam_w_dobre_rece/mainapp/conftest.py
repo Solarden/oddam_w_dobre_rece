9e9eed5875e5ae004a731a6565aa52ba7d9079c4
Pawel Ch <pchraczynski@gmail.com>
2021-12-04 20:01:48 +0100

    tests WIP

--- oddam_w_dobre_rece/mainapp/conftest.py
+++ oddam_w_dobre_rece/mainapp/conftest.py
@@ -1,8 +1,27 @@
 import pytest
-from .models import SiteUser
+from .models import SiteUser, Institution, Category
 from django.contrib.auth.models import Permission, Group
 
 
 @pytest.fixture
 def site_user():
-    return SiteUser.objects.create(username='testowy')
+    return SiteUser.objects.create(username='testowyj')
+
+
+@pytest.fixture
+def site_user_pwd():
+    user = SiteUser.objects.create(username='testowy', email='test@test.pl')
+    user.set_password('Tymczasowe1!')
+    return user
+
+@pytest.fixture
+def add_category():
+    return Category.objects.create(name='test')
+
+
+@pytest.fixture
+def add_institution(add_category):
+    a = Institution.objects.create(name='testowa', description='test', city_location='test')
+    a.categories.set(add_category)
+    a.save()
+    return a
--- oddam_w_dobre_rece/mainapp/tests.py
+++ oddam_w_dobre_rece/mainapp/tests.py
@@ -3,8 +3,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.test import Client
 from django.urls import reverse
 
-
 # Create your tests here.
+from mainapp.models import SiteUser
+
 
 @pytest.mark.django_db
 def test_landing_page():
@@ -28,7 +29,25 @@ def test_add_donation_with_login(site_user):
     assert response.status_code == 200
 
 
-# TODO add_donation_post
+@pytest.mark.django_db
+def test_add_donation_with_login_post(add_institution, site_user):
+    client = Client()
+    a = {
+        'quantity': '3',
+        'institution': add_institution,
+        'address': 'test',
+        'phone_no': '123',
+        'city': 'test',
+        'zip_code': '12',
+        'pick_up_date': '2021-01-01',
+        'pick_up_time': '12:12',
+        'pick_up_comment': 'test',
+        'user': site_user,
+    }
+    response = client.post(reverse("add_donation"), data=a)
+    assert response.status_code == 302
+    SiteUser.objects.get(**a)
+
 
 @pytest.mark.django_db
 def test_login():
@@ -37,7 +56,16 @@ def test_login():
     assert response.status_code == 200
 
 
-# TODO login post
+@pytest.mark.django_db
+def test_login_post(site_user_pwd):
+    client = Client()
+    b = {
+        'email': 'test@test.pl',
+        'password': 'Tymczasowe1!',
+    }
+    response = client.post(reverse("login"), data=b)
+    assert response.status_code == 302
+
 
 @pytest.mark.django_db
 def test_logout_no_login():
@@ -61,10 +89,37 @@ def test_register():
     assert response.status_code == 200
 
 
-# TODO register post
+@pytest.mark.django_db
+def test_register_post():
+    client = Client()
+    a = {
+        'name': 'test',
+        'surname': 'testowy',
+        'email': 'test@test.pl',
+        'password': 'Tymczasowe1!',
+        'password2': 'Tymczasowe1!',
+    }
+    response = client.post(reverse("register"), data=a)
+    assert response.status_code == 302
+
 
 @pytest.mark.django_db
 def test_activate_user():
     client = Client()
     response = client.get(reverse('register'))
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_user_info_no_login():
+    client = Client()
+    response = client.get(reverse('user'))
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_user_info_with_login(site_user):
+    client = Client()
+    client.force_login(site_user)
+    response = client.get(reverse('user'))
+    assert response.status_code == 302
