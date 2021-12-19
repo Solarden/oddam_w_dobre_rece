6ab047212e1ca348a7b03908e9edd8b63a7d3d4e
solarden <pchraczynski@gmail.com>
2021-12-19 12:26:56 +0100

    project deploy conf

--- oddam_w_dobre_rece/settings.py
+++ oddam_w_dobre_rece/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'mainapp/static')
 STATIC_URL = '/static/'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
